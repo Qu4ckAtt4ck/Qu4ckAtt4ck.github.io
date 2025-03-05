6e84f4b7de5117c9d7375351def2af4c15d5c172
QuackAttack <151987714+Qu4ckAtt4ck@users.noreply.github.com>
2025-03-05 14:13:10 -0600

    Update settings.js

--- site1/settings.js
+++ site1/settings.js
@@ -1,4 +1,4 @@
-document.getElementById('settings-btn').addEventListener('click', () => {
+document.getElementById('settings-button').addEventListener('click', () => {
     document.getElementById('settings-menu').style.display = 'block';
 });
 document.getElementById('close-settings').addEventListener('click', () => {
