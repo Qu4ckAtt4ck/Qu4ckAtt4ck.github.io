5ae49a2f70732dc1a2188f86d1cbc5305ec2a075
QuackAttack <151987714+Qu4ckAtt4ck@users.noreply.github.com>
2025-03-05 14:14:06 -0600

    Update cosmetics.js

--- site1/cosmetics.js
+++ site1/cosmetics.js
@@ -1,4 +1,4 @@
-document.getElementById('cosmetics-btn').addEventListener('click', () => {
+document.getElementById('cosmetics-button').addEventListener('click', () => {
     document.getElementById('cosmetics-menu').style.display = 'block';
 });
 document.getElementById('close-cosmetics').addEventListener('click', () => {
