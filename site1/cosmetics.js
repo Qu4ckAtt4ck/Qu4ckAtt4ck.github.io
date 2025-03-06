53ecc2b3f607b29d77dffeb7292347e18b2a77e0
QuackAttack <151987714+Qu4ckAtt4ck@users.noreply.github.com>
2025-03-06 08:24:08 -0600

    Delete site1/cosmetics.js

--- site1/cosmetics.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('cosmetics-button').addEventListener('click', () => {
-    document.getElementById('cosmetics-menu').style.display = 'block';
-});
-document.getElementById('close-cosmetics').addEventListener('click', () => {
-    document.getElementById('cosmetics-menu').style.display = 'none';
-});
