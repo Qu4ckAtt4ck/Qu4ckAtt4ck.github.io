28f32b56e60c1c58da9eaa569f1e61d6b9b6a6e7
QuackAttack <151987714+Qu4ckAtt4ck@users.noreply.github.com>
2025-03-06 08:28:25 -0600

    Delete site1/settings.js

--- site1/settings.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('settings-button').addEventListener('click', () => {
-    document.getElementById('settings-menu').style.display = 'block';
-});
-document.getElementById('close-settings').addEventListener('click', () => {
-    document.getElementById('settings-menu').style.display = 'none';
-});
